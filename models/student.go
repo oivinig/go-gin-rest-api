c62ec769378fb28a601773d29d7120f27a3f17d2
oivinig <vghffmnn@gmail.com>
2022-11-02 18:13:48 -0300

    feat: add document validation for students

--- models/student.go
+++ models/student.go
@@ -1,12 +1,20 @@
 package models
 
 import (
+	"gopkg.in/validator.v2"
 	"gorm.io/gorm"
 )
 
 type Student struct {
 	gorm.Model
-	StudentName string `json:"student_name"`
-	Document    string `json:"document"`
-	Registry    string `json:"registry"`
+	StudentName string `json:"student_name" validate:"nonzero"`
+	Document    string `json:"document" validate:"len=11, regexp=^[0-9]*$"`
+	Registry    string `json:"registry" validate:"len=9, regexp=^[0-11]*$"`
+}
+
+func ValidadeStudent(s *Student) error {
+	if err := validator.Validate(s); err != nil {
+		return err
+	}
+	return nil
 }
